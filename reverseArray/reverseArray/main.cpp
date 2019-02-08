76ff5e4d0a4d689213b47679b435c6d932beefd9
Pyerce <propyerce@gmail.com>
2019-02-07 19:39:16 -0800

    cpp file for function

--- reverseArray/reverseArray/main.cpp
+++ reverseArray/reverseArray/main.cpp
@@ -9,33 +9,33 @@
 #include <iostream>
 using namespace std;
 
-void revereseArray(int arr[], int start, int end)
+void revereseArray(int array[], int start, int end)
 {
     while (start < end)
     {
-        int temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
+        int temp = array[start];
+        array[start] = array[end];
+        array[end] = temp;
         start++;
         end--;
     }
 }
 
-void printArray(int arr[], int size)
+void printArray(int array[], int size)
 {
     for (int i = 0; i < size; i++)
-        cout << arr[i] << " ";
+        cout << array[i] << " ";
     
     cout << endl;
 }
 
 int main()
 {
-    int arr[] = {1, 2, 3, 4};
-    printArray(arr, 4);
-    revereseArray(arr, 0, 3);
+    int array[] = {1, 2, 3, 4};
+    printArray(array, 4);
+    revereseArray(array, 0, 3);
     cout << "Reversed array is" << endl;
-    printArray(arr, 4);
+    printArray(array, 4);
     
     return 0;
 }
